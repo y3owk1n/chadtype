bf4232f528fc30dea7f5810425b094d8a517b39c
y3owk1n <wongyeowkin@gmail.com>
2023-05-29 14:55:00 +0800

    init

---
+++ prettier.config.cjs
@@ -0,0 +1,27 @@
+/** @type {import("prettier").Config} */
+const config = {
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: true,
+    singleQuote: false,
+    bracketSpacing: true,
+    jsxSingleQuote: false,
+    printWidth: 80,
+    arrowParens: "always",
+    singleAttributePerLine: true,
+    importOrder: [
+        "^@(mda|ee)/(.*)$",
+        "^@lib/(.*)$",
+        "^@components/(.*)$",
+        "^@(server|trpc)/(.*)$",
+        "^~/(.*)$",
+        "^[./]",
+    ],
+    importOrderSeparation: true,
+    plugins: [
+        require.resolve("@trivago/prettier-plugin-sort-imports"),
+        require.resolve("prettier-plugin-tailwindcss"),
+    ],
+};
+
+module.exports = config;
---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import "@/styles/globals.css";
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import { type AppType } from "next/app";
+
+const MyApp: AppType<{ session: Session | null }> = ({
+    Component,
+    pageProps: { session, ...pageProps },
+}) => {
+    return (
+        <SessionProvider session={session}>
+            <Component {...pageProps} />
+        </SessionProvider>
+    );
+};
+
+export default MyApp;
