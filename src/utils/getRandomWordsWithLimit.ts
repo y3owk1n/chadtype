42de74da78bc8b4952f04d56c0acf61b3dfe5897
y3owk1n <wongyeowkin@gmail.com>
2023-05-31 01:56:03 +0800

    refactor: optimise getRandomWordsWithLimit function

--- src/utils/getRandomWordsWithLimit.ts
+++ src/utils/getRandomWordsWithLimit.ts
@@ -5,20 +5,13 @@ export function getRandomWordsWithLimit(
     array: ArrayType[],
     count: number
 ): ArrayType[] {
-    const randomWords: ArrayType[] = [];
+    const randomWords: ArrayType[] = array.slice(0, count);
 
-    // Fill the reservoir with the first 'count' elements
-    for (let i = 0; i < count; i++) {
-        randomWords[i] = array[i];
-    }
-
-    // Replace elements in the reservoir with decreasing probability
     for (let i = count; i < array.length; i++) {
         const j = Math.floor(Math.random() * (i + 1));
 
-        // If j is within the range of the reservoir, replace the element
         if (j < count) {
-            randomWords[j] = array[i];
+            randomWords[j] = array[i] as ArrayType;
         }
     }
 
