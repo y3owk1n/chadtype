ec2c5475e16a76ccbe0b2cc76b9d0955d32264ae
y3owk1n <wongyeowkin@gmail.com>
2023-06-02 02:08:00 +0800

    feat: add basic seo tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { MainNav, buttonVariants } from "@/components";
 import { SiteFooter } from "@/components/molecules/SiteFooter";
 import { NextAuthProvider, ThemeProvider } from "@/components/providers";
+import { siteConfig } from "@/lib";
 import "@/styles/globals.css";
 import { cn } from "@/utils";
 import { Inter as FontSans } from "next/font/google";
@@ -10,6 +11,46 @@ const fontSans = FontSans({
     subsets: ["latin"],
     variable: "--font-sans",
 });
+
+export const metadata = {
+    title: {
+        default: siteConfig.name,
+        template: `%s | ${siteConfig.name}`,
+    },
+    description: siteConfig.description,
+    keywords: ["Typing", "Game", "Type", "Speed"],
+    authors: [
+        {
+            name: "kyle",
+            url: "https://kylewong.my",
+        },
+    ],
+    creator: "kyle",
+    themeColor: [
+        { media: "(prefers-color-scheme: light)", color: "white" },
+        { media: "(prefers-color-scheme: dark)", color: "black" },
+    ],
+    openGraph: {
+        type: "website",
+        locale: "en_US",
+        url: siteConfig.url,
+        title: siteConfig.name,
+        description: siteConfig.description,
+        siteName: siteConfig.name,
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: siteConfig.name,
+        description: siteConfig.description,
+        images: [`${siteConfig.url}/og.jpg`],
+    },
+    icons: {
+        icon: "/favicon.ico",
+        shortcut: "/favicon-16x16.png",
+        apple: "/apple-touch-icon.png",
+    },
+    manifest: `${siteConfig.url}/site.webmanifest`,
+};
 
 export default function RootLayout({
     children,
