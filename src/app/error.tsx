ecac3b427a2ec5469c1256bbe693c6cc22a6a9bb
y3owk1n <wongyeowkin@gmail.com>
2023-06-01 12:01:42 +0800

    feat: add error handle page

---
+++ src/app/error.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { Button, buttonVariants } from "@/components";
+import { cn } from "@/utils";
+import Link from "next/link";
+
+export default function Error({
+    error,
+    reset,
+}: {
+    error: Error;
+    reset: () => void;
+}) {
+    return (
+        <div className="grid h-full min-h-[calc(100vh-80px-96px)] place-items-center">
+            <div className="grid max-w-[42rem] gap-8 text-center">
+                <div className="grid gap-4">
+                    <h1 className="text-4xl font-bold">
+                        Ooppssss... an error occured.
+                    </h1>
+                    <p className="text-muted-foreground">{error.message}</p>
+                </div>
+                <div className="mx-auto flex gap-4">
+                    <Link
+                        href="/"
+                        className={cn(
+                            buttonVariants({
+                                variant: "outline",
+                            })
+                        )}
+                    >
+                        Back To Home
+                    </Link>
+                    <Button onClick={reset}>Try Again</Button>
+                </div>
+            </div>
+        </div>
+    );
+}
