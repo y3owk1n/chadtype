2fdfcb444f795f0cf25fa5af078358821c2299e9
y3owk1n <wongyeowkin@gmail.com>
2023-06-01 10:55:52 +0800

    fix: site footer wrong links on github account and repo

--- src/components/molecules/SiteFooter.tsx
+++ src/components/molecules/SiteFooter.tsx
@@ -13,7 +13,7 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
                     <p className="text-center text-sm leading-loose md:text-left">
                         Built by{" "}
                         <a
-                            href={siteConfig.links.repo}
+                            href={siteConfig.links.github}
                             target="_blank"
                             rel="noreferrer"
                             className="font-medium underline underline-offset-4"
@@ -31,7 +31,7 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
                         </a>
                         . The source code is available on{" "}
                         <a
-                            href={siteConfig.links.github}
+                            href={siteConfig.links.repo}
                             target="_blank"
                             rel="noreferrer"
                             className="font-medium underline underline-offset-4"
