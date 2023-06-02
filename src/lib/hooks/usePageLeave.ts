12a3ac658a68de073160933c72a586c0c030d8b5
y3owk1n <wongyeowkin@gmail.com>
2023-06-03 02:49:13 +0800

    feat: add usePageLeave hook

---
+++ src/lib/hooks/usePageLeave.ts
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+
+export function usePageLeave(onPageLeave: () => void) {
+    useEffect(() => {
+        document.documentElement.addEventListener("mouseleave", onPageLeave);
+        return () =>
+            document.documentElement.removeEventListener(
+                "mouseleave",
+                onPageLeave
+            );
+    }, [onPageLeave]);
+}
