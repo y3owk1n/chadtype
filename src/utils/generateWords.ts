93a00c1c9628f1e747f6bbc6f74f58703a1e2c84
y3owk1n <wongyeowkin@gmail.com>
2023-06-03 01:35:15 +0800

    chore: remove deprecated generatewords file

--- src/utils/generateWords.ts
+++
@@ -1,6 +0,0 @@
-import { faker } from "@faker-js/faker";
-
-export function generateWords({ count = 10 }: { count: number }): string {
-    const generatedWordsString = faker.word.words({ count });
-    return generatedWordsString;
-}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,4 @@
 export * from "./cn";
-export * from "./generateWords";
 export * from "./htmlToString";
 export * from "./removeNonEnglishCharacters";
 export * from "./arrayToKvObject";
