dfc3407a3db9d312398a6b8ac9254fbbc8d681ac
y3owk1n <wongyeowkin@gmail.com>
2023-06-08 10:52:45 +0800

    feat: add login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+    title: "Login",
+};
+
+export default function Page() {
+    return (
+        <div className="grid h-full min-h-[calc(100vh-80px-96px)] place-items-center">
+            <div className=" grid w-full max-w-4xl gap-8 ">Coming Soon</div>
+        </div>
+    );
+}
