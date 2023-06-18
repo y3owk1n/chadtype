4f0ec8093a9905d14431c45e5d671e6a843be25d
y3owk1n <wongyeowkin@gmail.com>
2023-06-18 23:46:06 +0800

    fix: average function returns 0 instead of NAN

--- src/utils/getAverage.ts
+++ src/utils/getAverage.ts
@@ -1,4 +1,7 @@
 export function getAverage(numbers: number[]): number {
+    if (numbers.length <= 0) {
+        return 0;
+    }
     const sum = numbers.reduce((acc, curr) => acc + curr, 0);
     return sum / numbers.length;
 }
